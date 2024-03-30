f278a4248763587fbdb96c8e9b242f8c1730a083
Çağrı KART <ckarte43@gmail.com>
2024-03-30 13:15:55 +0300

    add hakkimizda page

---
+++ mfcworkmvc/Controllers/Hakkimizda.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mfcworkmvc.Controllers
+{
+    public class Hakkimizda : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
